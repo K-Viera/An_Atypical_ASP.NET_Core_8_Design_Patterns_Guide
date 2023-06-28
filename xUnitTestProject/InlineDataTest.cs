9b1441fcd9d04b08ca2542a7abc581a6e487d9c4
K-Viera <kevin.viera.989@gmail.com>
2023-06-28 00:42:11 -0500

    InlineData an MemberData

---
+++ xUnitTestProject/InlineDataTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTestProject
+{
+    public class InlineDataTest
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(5, 5)]
+        public void Should_be_equal(int value1, int value2)
+        {
+            Assert.Equal(value1, value2);
+        }
+    }
+}
