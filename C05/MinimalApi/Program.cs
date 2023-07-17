50467013bae401f07ff536619f9af8dfb907fc0c
K-Viera <kevin.viera.989@gmail.com>
2023-07-17 00:45:39 -0500

    Output data

--- C05/MinimalApi/Program.cs
+++ C05/MinimalApi/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics.CodeAnalysis;
 
 var builder = WebApplication.CreateBuilder(args); 
 var app = builder.Build(); 
+
+#region Inputs
+
 app.MapGet("minimal-endpoint-inline", () => "GET!");
 
 app.MapGet( 
@@ -36,6 +40,65 @@ app.MapGet(
     "minimal-endpoint-input-Person/",
     (Person person) => person
 );
+app.MapGet( 
+    "minimal-endpoint-input-Person2/", 
+    ([AsParameters] Person2 person) => person 
+); 
+
+#endregion
+
+#region Outputs
+
+app.MapGet( 
+    "minimal-endpoint-output-coordinate/", 
+    () => new Coordinate { 
+        Latitude = 43.653225, 
+        Longitude = -79.383186 
+    } 
+); 
+
+app.MapGet("minimal-endpoint-output-coordinate-ok1/", () =>
+    Results.Ok(new Coordinate {
+        Latitude = 43.653225,
+        Longitude = -79.383186
+    })
+);
+
+app.MapGet("minimal-endpoint-output-coordinate-ok2/", () =>
+    TypedResults.Ok(new Coordinate {
+        Latitude = 43.653225,
+        Longitude = -79.383186
+    })
+);
+
+app.MapGet(
+    "multiple-TypedResults/",
+    Results<Ok, Conflict> ()
+        => Random.Shared.Next(0, 100) % 2 == 0
+            ? TypedResults.Ok()
+            : TypedResults.Conflict()
+);
+
+app.MapGet(
+    "multiple-TypedResults-delegate/{number}",
+    (int number)=>MultipleResultsDelegate(number)
+);
+
+Results<Ok, Conflict> MultipleResultsDelegate(int number)
+{
+    return number % 2 == 0
+        ? TypedResults.Ok()
+        : TypedResults.Conflict();
+}
+
+#endregion
+
 
 app.Run();
+
+public class Person2 
+{ 
+    public required string Name { get; set; } 
+    public required DateOnly Birthday { get; set; } 
+} 
 
