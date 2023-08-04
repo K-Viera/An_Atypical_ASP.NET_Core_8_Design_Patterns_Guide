c51c0e62dc965e8c2459f0103f5b25cb3b840426
K-Viera <kevin.viera.989@gmail.com>
2023-08-03 23:04:16 -0500

    Dependency Injection with Extension methods

---
+++ C08/DependencyInjection/DemoFeatureExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CompositionRoot.DemoFeature;
+namespace Microsoft.Extensions.DependencyInjection;
+public static class DemoFeatureExtensions
+{
+    public static IServiceCollection AddDemoFeatures(this IServiceCollection services)
+    {
+        return services
+            .AddSingleton<MyFeature>()
+            .AddSingleton<IMyFeatureDependency, MyFeatureDependency>()
+            ;
+    }
+}
