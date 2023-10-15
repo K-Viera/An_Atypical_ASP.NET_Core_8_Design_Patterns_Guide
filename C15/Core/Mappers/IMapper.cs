01365d0fd9e57fdb903fba0f8a2ecea646a7aa40
K-Viera <kevin.viera.989@gmail.com>
2023-10-15 17:58:04 -0500

    C15 and mapping objects properties

---
+++ C15/Core/Mappers/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappers
+{
+    public interface IMapper<TSource,TDestination>
+    {
+        TDestination Map(TSource entity);
+    }
+}
