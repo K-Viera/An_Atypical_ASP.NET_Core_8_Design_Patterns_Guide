04dfec37b753f8982577cabbc8f91d7f7e9881ed
K-Viera <kevin.viera.989@gmail.com>
2023-10-16 21:03:37 -0500

    AutoMapper

---
+++ C15/AutoMapperWeb/WebProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Core.Models;
+using Core;
+
+namespace MapperWeb
+{
+    public class WebProfile : Profile
+    {
+        public WebProfile()
+        {
+            CreateMap<Product, ProductDetails>();
+            CreateMap<NotEnoughStockException, NotEnoughStock>();
+            CreateMap<ProductNotFoundException, ProductNotFound>();
+        }
+
+    }
+}
